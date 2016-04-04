a313afc705b772c03757a7b0c5039947ced168aa
Bodz <jean.boulot@epitech.eu>
2016-04-04 10:01:54 +0200

    controller, first try

---
+++ Assets/Scripts/Controller/Controller.cs
@@ -0,0 +1,125 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Controller : MonoBehaviour 
+{
+    public float lateralSpeed = 5f;
+    public float jumpSpeed = 3f;
+
+    public int playerNumber;
+
+    private Rigidbody2D _rgbd2d;
+    private JoyStickManager _jsm;
+
+    [SerializeField]
+    private Transform[] ItemCorner; // in this order : topleft, topright, bottomleft, bottomright
+    private Collider2D[] _colliders;
+
+    private bool _grounded;
+    public bool Grounded
+    {
+        get { return _grounded; }
+        private set 
+        {
+            if (_grounded != value)
+            {
+                if (value == true) // the player have just touched the ground
+                {
+                    _jumpcharges = MaxJumpCharges;
+                }
+                else // the player has left the ground, either by jumping or falling -> it is counted as a jump
+                {
+                    _jumpcharges--;
+                }
+            }
+            _grounded = value;
+        }
+    }
+    
+    // the number of jump the player can do (2 by default : basic jump and air jump)
+    private int _jumpcharges = 2;
+
+    /// <summary>
+    /// if we want to add more air jump.
+    /// </summary>
+    private int _maxJumpCharges;
+    public int MaxJumpCharges
+    {
+        get { return _maxJumpCharges; }
+        set { _maxJumpCharges = value; }
+    }
+
+    void Awake()
+    {
+        _rgbd2d = GetComponent<Rigidbody2D>();
+        _jsm = new JoyStickManager(playerNumber);
+
+        _colliders = this.GetComponents<Collider2D>().Where(x => x.isTrigger == false).ToArray();
+    }
+
+    void Start()
+    {
+        _maxJumpCharges = _jumpcharges;
+    }
+
+    void FixedUpdate()
+    {
+        float xVel, yVel;
+
+        xVel = _jsm.GetAxisClamped(JoyStickManager.e_XBoxControllerAxis.Horizontal) * lateralSpeed;
+
+        if (_jumpcharges > 0 && _jsm.GetButtonDown(JoyStickManager.e_XBoxControllerButtons.A))
+        {
+            if (!Grounded)
+                _jumpcharges--;
+            yVel = jumpSpeed;
+        }
+        else
+        {
+            yVel = _rgbd2d.velocity.y;
+        }
+        jumpHelper();
+
+        _rgbd2d.velocity = new Vector2(xVel, yVel);
+
+        //Debug.Log("A activated : " + _jsm.GetButtonDown(JoyStickManager.e_XBoxControllerButtons.B));
+        //Debug.Log("A activated : " + Input.GetButtonDown("B1"));
+    }
+
+    private void jumpHelper()
+    {
+        RaycastHit2D hit, hit2;
+
+        hit = Physics2D.Linecast(ItemCorner[2].position, ItemCorner[3].position, 1 << LayerMask.NameToLayer("Platform"));
+        hit2 = Physics2D.Linecast(ItemCorner[2].position + Vector3.up * 0.02f, ItemCorner[3].position + Vector3.up * 0.02f, 1 << LayerMask.NameToLayer("Platform"));
+        //Debug.Log("hit : " + hit.collider);
+
+        Grounded = hit.collider != null && hit2.collider == null;
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        foreach (Collider2D collider in _colliders)
+        {
+            Physics2D.IgnoreCollision(col, collider, true);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        foreach (Collider2D collider in _colliders)
+        {
+            Physics2D.IgnoreCollision(col, collider, false);
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(ItemCorner[2].position + Vector3.up * 0.02f, ItemCorner[3].position + Vector3.up * 0.02f);
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawLine(ItemCorner[2].position, ItemCorner[3].position);
+    }
+}
---
+++ Assets/Scripts/Controller/JoyStickManager.cs
@@ -0,0 +1,129 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class JoyStickManager
+{
+    public enum e_XBoxControllerAxis
+    {
+        Horizontal,
+        Vertical,
+        HCross,
+        VCross,
+        HJoyStick,
+        VJoyStick
+    }
+
+    public enum e_XBoxControllerButtons
+    {
+        A,
+        B,
+        Y,
+        X,
+        Start,
+        Back,
+        RB,
+        LB,
+        LeftJSClick,
+        RightJSClick,
+        RT,
+        LT
+    }
+
+    [SerializeField]
+    private int _joystickId;
+    public int JoystickId
+    {
+        get { return _joystickId; }
+        set 
+        {
+            _joystickId = value;
+            setMaps(value.ToString());
+        }
+    }
+
+    private Dictionary<e_XBoxControllerAxis, string> _givenNameToAxis;
+    private Dictionary<e_XBoxControllerButtons, string> _givenNameToButton;
+
+    public JoyStickManager(int id)
+    {
+        _givenNameToAxis = new Dictionary<e_XBoxControllerAxis, string>();
+        _givenNameToButton = new Dictionary<e_XBoxControllerButtons, string>();
+        JoystickId = id;
+    }
+
+    /// <summary>
+    /// Axis are : 
+    /// Horizontal (left joystick, left/right)
+    /// Vertical (left joystick, up/down)
+    /// HCross (cross, left/right)
+    /// VCross (cross, up/down)
+    /// HJoystick (right joystick, left/right)
+    /// VJoystick (right joystick, up/down)
+    /// </summary>
+    /// <param name="name"></param>
+    /// <returns></returns>
+    public float GetAxis(e_XBoxControllerAxis axis)
+    {       
+        return Input.GetAxis(_givenNameToAxis[axis]);
+    }
+
+    /// <summary>
+    /// the same as GetAxis, but instead of returning a value clamped in [-1;1], it only returns {-1, 0, 1}
+    /// </summary>
+    /// <param name="name"></param>
+    /// <returns></returns>
+    public int GetAxisClamped(e_XBoxControllerAxis axis)
+    {
+        var val = Input.GetAxis(_givenNameToAxis[axis]);
+        if (val < 0)
+            return -1;
+        else if (val > 0)
+            return 1;
+        return 0;
+    }
+
+    /// <summary>
+    /// names are : 
+    /// A, B, Y, X, Start, Back, RB, RT, LB, LT, LeftJSClick, RightJSClick
+    /// </summary>
+    /// <param name="name"></param>
+    /// <returns></returns>
+    public bool GetButtonDown(e_XBoxControllerButtons btn)
+    {
+        if (btn == e_XBoxControllerButtons.RT || btn == e_XBoxControllerButtons.LT)
+            return Input.GetAxis(_givenNameToButton[btn]) != 0;
+        return Input.GetButtonDown(_givenNameToButton[btn]);
+    }
+
+    public bool GetButtonUp(e_XBoxControllerButtons btn)
+    {
+        if (btn == e_XBoxControllerButtons.RT || btn == e_XBoxControllerButtons.LT) 
+            Debug.LogError("RT and LT releasing is not managed.");
+
+        return Input.GetButtonUp(_givenNameToButton[btn]);
+    }
+
+    private void setMaps(string id)
+    {
+        _givenNameToAxis[e_XBoxControllerAxis.Horizontal] = "Horizontal" + id;
+        _givenNameToAxis[e_XBoxControllerAxis.Vertical] = "Vertical" + id;
+        _givenNameToAxis[e_XBoxControllerAxis.HCross] = "HCross" + id;
+        _givenNameToAxis[e_XBoxControllerAxis.VCross] = "VCross" + id;
+        _givenNameToAxis[e_XBoxControllerAxis.HJoyStick] = "HJoystick" + id;
+        _givenNameToAxis[e_XBoxControllerAxis.VJoyStick] = "VJoystick" + id;
+
+        _givenNameToButton[e_XBoxControllerButtons.A] = "A" + id;
+        _givenNameToButton[e_XBoxControllerButtons.B] = "B" + id;
+        _givenNameToButton[e_XBoxControllerButtons.Y] = "Y" + id;
+        _givenNameToButton[e_XBoxControllerButtons.X] = "X" + id;
+        _givenNameToButton[e_XBoxControllerButtons.Start] = "Start" + id;
+        _givenNameToButton[e_XBoxControllerButtons.Back] = "Back" + id;
+        _givenNameToButton[e_XBoxControllerButtons.RB] = "RB" + id;
+        _givenNameToButton[e_XBoxControllerButtons.LB] = "LB" + id;
+        _givenNameToButton[e_XBoxControllerButtons.LeftJSClick] = "LeftJSClick" + id;
+        _givenNameToButton[e_XBoxControllerButtons.RightJSClick] = "RightJSCLick" + id;
+        _givenNameToButton[e_XBoxControllerButtons.RT] = "RT" + id;
+        _givenNameToButton[e_XBoxControllerButtons.LT] = "LT" + id;
+    }
+}
