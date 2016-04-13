15354dca62ffaa6245f86e35229ce7ddb68a5daa
gaier_v <valentin.gaier@epitech.eu>
2016-04-13 23:35:30 +0900

    add script for checking death and scrolling cam

---
+++ Assets/Scripts/Camera/CamSteppingUp.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamSteppingUp : MonoBehaviour
+{
+
+    public GameObject Player;
+    public GameObject stepUpTrigger;
+    private Transform playerPos;
+    private Transform triggerPos;
+    private float time;
+    Vector3 velo = Vector3.zero;
+
+    // Use this for initialization
+    void Start()
+    {
+        time = 0.3f;
+
+    }
+
+    void StepUpCam()
+    {
+        playerPos = Player.GetComponent<Transform>();
+        triggerPos = stepUpTrigger.GetComponent<Transform>();
+        if (playerPos.position.y > triggerPos.position.y)
+        {
+            transform.position = Vector3.SmoothDamp(transform.position, transform.position + new Vector3(0, 5, 0), ref velo, time);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StepUpCam();
+    }
+}
