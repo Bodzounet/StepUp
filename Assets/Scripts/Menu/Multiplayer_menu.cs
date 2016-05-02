80f40621e5d97d4a3eb1353ca5e0a17bb4c4b92a
Damien Blanc <damien.blanc@epitech.eu>
2016-05-02 15:07:03 +0900

    .

--- Assets/Scripts/Menu/Multiplayer_menu.cs
+++ Assets/Scripts/Menu/Multiplayer_menu.cs
@@ -4,30 +4,37 @@ using System.Collections.Generic;
 using UnityEngine.UI;
 
 public class Multiplayer_menu : MonoBehaviour {
-    private Dictionary<int, string> _startsBtn = new Dictionary<int, string>() {{ 1, "Start1"}, { 2, "Start2"}, { 3, "Start3"}, { 4, "Start4"}};
-    private List<int>               _connectedPlayer = new List<int>() { };
+    private Dictionary<int, string> _startsBtn;
+    private List<int>               _connectedPlayer;
+    SharedObject                    _sharedObject;
 
-    private void AddCotroller(int id)
+    private void AddController(int id)
     {
         _connectedPlayer.Add(id);
         GameObject.Find("Player" + id + "Status").GetComponent<Text>().text = "Connected";
     }
 
-    private void RemoveCotroller(int id)
+    private void RemoveController(int id)
     {
         _connectedPlayer.Remove(id);
         GameObject.Find("Player" + id + "Status").GetComponent<Text>().text = "Press Start...";
     }
 
-    // Update is called once per frame
+    void Start ()
+    {
+        _startsBtn = new Dictionary<int, string>() { { 1, "Start1" }, { 2, "Start2" }, { 3, "Start3" }, { 4, "Start4" } };
+        _connectedPlayer = new List<int>() { };
+        _sharedObject = GameObject.Find("SharedObject").GetComponent<SharedObject>();
+    }
+
     void Update () {
         foreach (KeyValuePair<int, string> entry in _startsBtn)
             if (Input.GetButtonDown(entry.Value))
             {
                 if (!_connectedPlayer.Exists(x => x == entry.Key))
-                    AddCotroller(entry.Key);
+                    AddController(entry.Key);
                 else
-                    RemoveCotroller(entry.Key);
+                    RemoveController(entry.Key);
             }
 
         //GameObject.Find("PanelMultiplayer/Play").SetActive(_connectedPlayer.Count >= 2);
