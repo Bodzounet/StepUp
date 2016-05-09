bcfbabff11b1a0ce2ae9d1ae092f0c73f26f2b82
Tamer <lucas.hertzog@epitech.eu>
2016-05-09 08:53:23 +0200

    Adding the destructible path fills you with determination

---
+++ Assets/Scripts/Platforms/RemoveOnPass.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RemoveOnPass : MonoBehaviour {
+
+    public GameObject[] toRemove;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            for (int i = 0; i < toRemove.Length; i++)
+            {
+                if (toRemove[i] != null)
+                    Object.Destroy(toRemove[i]);
+            }
+        }
+    }
+}
