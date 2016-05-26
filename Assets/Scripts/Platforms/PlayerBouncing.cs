6464fef0648195e8f74dc7137d11f506d8ce4e6b
Nicholas Grant <seigneurfarfadet@hotmail.fr>
2016-05-26 10:33:51 +0900

    bouncing paltform

---
+++ Assets/Scripts/Platforms/PlayerBouncing.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBouncing : MonoBehaviour {
+
+    private Animator animator;
+	// Use this for initialization
+	void Start () {
+        animator = this.GetComponent<Animator>();
+	}
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+            animator.Play("bouncing_platform");
+    }
+}
